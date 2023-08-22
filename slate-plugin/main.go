dda16a7ed8b7905601d7f3a56c864656aeb7da54
Aditya Prerepa <adiprerepa@gmail.com>
2023-08-21 23:11:47 -0500

    override headers when hit threshold

--- slate-plugin/main.go
+++ slate-plugin/main.go
@@ -3,21 +3,27 @@ package main
 import (
 	"encoding/binary"
 	"errors"
+	"fmt"
 	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm"
 	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm/types"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
 const (
-	KEY_REQUEST_COUNT = "slate_request_count"
-	KEY_LAST_RESET    = "slate_last_reset"
-	KEY_RPS           = "slate_rps_threshold"
+	KEY_REQUEST_COUNT  = "slate_request_count"
+	KEY_LAST_RESET     = "slate_last_reset"
+	KEY_RPS_THRESHOLDS = "slate_rps_threshold"
 	// this is in millis
-	AGGREGATE_REQUEST_LATENCY = "slate_last_second_latency_avg"
+	AGGREGATE_REQUEST_LATENCY       = "slate_last_second_latency_avg"
+	TICK_PERIOD                     = 1000
+	SLATE_REMOTE_CLUSTER_HEADER_KEY = "x-slate-remotecluster"
 )
 
 var (
-	ALL_KEYS = []string{KEY_REQUEST_COUNT, KEY_LAST_RESET, KEY_RPS, AGGREGATE_REQUEST_LATENCY}
+	ALL_KEYS = []string{KEY_REQUEST_COUNT, KEY_LAST_RESET, KEY_RPS_THRESHOLDS, AGGREGATE_REQUEST_LATENCY}
 )
 
 func main() {
@@ -28,6 +34,12 @@ type vmContext struct {
 	// Embed the default VM context here,
 	// so that we don't need to reimplement all the methods.
 	types.DefaultVMContext
+}
+
+type RpsThreshold struct {
+	Threshold uint64
+	// for key x-slate-remotecluster
+	HeaderValue string
 }
 
 // Override types.DefaultVMContext.
@@ -47,10 +59,12 @@ func (*vmContext) OnVMStart(vmConfigurationSize int) types.OnVMStartStatus {
 
 type pluginContext struct {
 	types.DefaultPluginContext
+
+	rpsThresholds []RpsThreshold
 }
 
 func (p *pluginContext) OnPluginStart(pluginConfigurationSize int) types.OnPluginStartStatus {
-	if err := proxywasm.SetTickPeriodMilliSeconds(1000); err != nil {
+	if err := proxywasm.SetTickPeriodMilliSeconds(TICK_PERIOD); err != nil {
 		proxywasm.LogCriticalf("unable to set tick period: %v", err)
 		return types.OnPluginStartStatusFailed
 	}
@@ -74,7 +88,7 @@ func (p *pluginContext) OnTick() {
 	lastReset := int64(binary.LittleEndian.Uint64(data))
 	currentNanos := time.Now().UnixMilli()
 	// allow for some jitter - this is bad and racy and hardcoded
-	if lastReset >= (currentNanos - 500) {
+	if lastReset >= (currentNanos - (TICK_PERIOD / 2)) {
 		// we've been reset. don't need to stream RPS
 		return
 	}
@@ -122,11 +136,21 @@ func (p *pluginContext) OnTick() {
 		latencyAvg = 0
 	}
 
+	service := os.Getenv("HOSTNAME")
+	if service == "" {
+		service = "SLATE_UNKNOWN"
+	}
+
 	// print ontick results
 	proxywasm.LogCriticalf("request count: %d, latency avg: %d", reqCount, latencyAvg)
-
-	// todo(adiprerepa) stream these results to the control plane
-
+	controllerHeaders := [][2]string{
+		{":method", "POST"},
+		{":path", "/proxyLoad"},
+		{":authority", "slate-controller.default.svc.cluster.local"},
+		{"x-slate-podname", service},
+	}
+	proxywasm.DispatchHttpCall("outbound|8000||slate-controller.default.svc.cluster.local", controllerHeaders,
+		[]byte(fmt.Sprintf("%d %d", reqCount, latencyAvg)), make([][2]string, 0), 5000, OnTickHttpCallResponse)
 }
 
 // Override types.DefaultPluginContext.
@@ -174,7 +198,35 @@ func (ctx *httpContext) OnHttpRequestHeaders(int, bool) types.Action {
 	}
 
 	// todo(adiprerepa): check if we've exceeded the threshold and set headers accordingly
+	//   right now, we can just do this linearly: if reqCount > threshold, set header. this resets every tick.
+	//   the downside is requests will not be evenly "distributed" - 0->thresh requests go to one pod, thresh->thresh2 go
+	//   to another, but sequentially, not uniformly.
 
+	data, _, err = proxywasm.GetSharedData(KEY_RPS_THRESHOLDS)
+	if err != nil {
+		proxywasm.LogCriticalf("Couldn't get shared data: %v", err)
+		return types.ActionContinue
+	}
+	// assume sorted - IN REVERSE ORDER
+	thresholds := ParseThresholds(string(data))
+
+	// maybe we don't need to get this twice.
+	data, _, err = proxywasm.GetSharedData(KEY_REQUEST_COUNT)
+	if err != nil {
+		proxywasm.LogCriticalf("Couldn't get shared data: %v", err)
+		return types.ActionContinue
+	}
+	reqCount = binary.LittleEndian.Uint64(data)
+
+	for _, thresh := range thresholds {
+		if reqCount > thresh.Threshold {
+			// set header
+			if err := proxywasm.AddHttpRequestHeader(SLATE_REMOTE_CLUSTER_HEADER_KEY, thresh.HeaderValue); err != nil {
+				proxywasm.LogCriticalf("Couldn't set request header SLATE_REMOTE_CLUSTER_HEADER_KEY: %v", err)
+				return types.ActionContinue
+			}
+		}
+	}
 	return types.ActionContinue
 }
 
@@ -211,5 +263,39 @@ func (ctx *httpContext) OnHttpStreamDone() {
 		proxywasm.LogCriticalf("unable to set shared data: %v", err)
 		return
 	}
-	
+
+}
+
+func OnTickHttpCallResponse(numHeaders, bodySize, numTrailers int) {
+	// receive RPS thresholds, set shared data accordingly
+	if bodySize == 0 {
+		return
+	}
+	respBody, err := proxywasm.GetHttpCallResponseBody(0, bodySize)
+	if err != nil {
+		proxywasm.LogCriticalf("Couldn't get http call response body: %v", err)
+		return
+	}
+	proxywasm.LogCriticalf("setting rps thresholds: %s", string(respBody))
+	// set thresholds
+	if err := proxywasm.SetSharedData(KEY_RPS_THRESHOLDS, respBody, 0); err != nil {
+		proxywasm.LogCriticalf("Couldn't set shared data for rps thresholds: %v", err)
+		return
+	}
+}
+
+func ParseThresholds(rawThresh string) (thresholds []RpsThreshold) {
+	for _, thresh := range strings.Split(rawThresh, "\n") {
+		rpsToHeaderRaw := strings.Split(thresh, " ")
+		if len(rpsToHeaderRaw) != 2 {
+			continue
+		}
+		rps, err := strconv.Atoi(rpsToHeaderRaw[0])
+		if err != nil {
+			continue
+		}
+		header := rpsToHeaderRaw[1]
+		thresholds = append(thresholds, RpsThreshold{Threshold: uint64(rps), HeaderValue: header})
+	}
+	return
 }
