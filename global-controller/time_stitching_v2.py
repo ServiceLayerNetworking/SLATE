32bb54de5be7d3bbd9ed2e004cf25a2f5c126469
gangmuk <gangmuk@gmail.com>
2023-09-17 22:45:10 -0500

    Add optimizer and time stitching. (not tested yet)

---
+++ global-controller/time_stitching_v2.py
@@ -0,0 +1,339 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import time
+from pprint import pprint
+
+# LOG_PATH = "./call-logs-sept-13.txt"
+# LOG_PATH = "./trace_and_load_log.txt"
+LOG_PATH = "./modified_trace_and_load_log.txt"
+# LOG_PATH = "./call-logs-sept-16.txt"
+
+VERBOSITY=1
+intra_cluster_network_rtt = 1
+inter_cluster_network_rtt = 1
+
+""" Trace exampe line (Version 1 wo call size)
+2
+f85116460cc0c607a484d0521e62fb19 7c30eb0e856124df a484d0521e62fb19 1694378625363 1694378625365
+4ef8ed533389d8c9ace91fc1931ca0cd 48fb12993023f618 ace91fc1931ca0cd 1694378625363 1694378625365
+
+<Num requests>
+<Trace Id> <Span Id> <Parent Span Id> <Start Time> <End Time>
+
+Root svc will have no parent span id
+"""
+        
+def print_log(msg, obj=None):
+    if VERBOSITY >= 1:
+        if obj == None:
+            print("[LOG] ", end="")
+            print(msg)
+        else:
+            print("[LOG] ", end="")
+            print(msg, obj)
+        
+def print_error(msg):
+    exit_time = 1
+    print("[ERROR] " + msg)
+    print("EXIT PROGRAM in")
+    for i in reversed(range(exit_time)) :
+        print("{} seconds...".format(i))
+        time.sleep(1)
+    assert False
+
+def file_read(path_):
+    f = open(path_, "r")
+    lines = f.readlines()
+    return lines
+
+
+class Span:
+    def __init__(self, svc, my_span_id, parent_span_id, st, et, load, cluster_id):
+        self.svc_name = svc
+        self.child_spans = list()
+        self.cluster_id = cluster_id
+        self.name_cid = self.svc_name+"#"+str(self.cluster_id)
+        self.root = (parent_span_id=="")
+        
+        self.my_span_id = my_span_id
+        self.parent_span_id = parent_span_id
+        
+        self.request_size_in_bytes = 10 # parent_span to this span
+        
+        self.st = st
+        self.et = et
+        self.rt = et - st
+        self.xt = 0
+        self.load = load
+    
+    def print(self):
+        print("SPAN,{},{},{}->{},{},{},{},{},{}".format(self.svc_name, self.cluster_id, self.parent_span_id, self.my_span_id, self.st, self.et, self.rt, self.xt, self.load))
+
+# <Trace Id> <Span Id> <Parent Span Id> <Start Time> <End Time>
+# <Parent Span Id> will not exist for the frontend service. e.g., productpage service in bookinfo
+# min len of tokens = 4
+
+SPAN_DELIM = " "
+
+if LOG_PATH == "./call-logs-sept-16.txt":
+    SPAN_TOKEN_LEN = 6
+else:
+    SPAN_TOKEN_LEN = 5
+
+def parse_and_create_span(line, svc, load):
+    tokens = line.split(SPAN_DELIM)
+    if len(tokens) != SPAN_TOKEN_LEN:
+        print_error("Invalid token length in span line. len(tokens):{}, line: {}".format(len(tokens), line))
+    tid = tokens[0]
+    sid = tokens[1]
+    psid = tokens[2]
+    st = int(tokens[3])
+    et = int(tokens[4])
+    span = Span(svc, sid, psid, st, et, load, -1)
+    return tid, span
+
+FRONTEND_svc = "productpage-v1"
+span_id_of_FRONTEND_svc = ""
+REVIEW_V1_svc = "reviews-v1"
+REVIEW_V2_svc = "reviews-v2"
+REVIEW_V3_svc = "reviews-v3"
+RATING_svc = "ratings-v1"
+DETAIL_svc = "details-v1"
+###############################
+FILTER_REVIEW_V1 = False # False
+FILTER_REVIEW_V2 = False # False
+FILTER_REVIEW_V3 = True # False
+###############################
+# ratings-v1 and reviews-v1 should not exist in the same trace
+MIN_TRACE_LEN = 3
+MAX_TRACE_LEN = 4
+def remove_incomplete_trace(traces_):
+    input_trace_len = len(traces_[cid])
+    removed_traces_ = list()
+    what = [0]*9
+    ret_traces_ = dict()
+    for cid, tid in traces_.items():
+        single_trace = traces_[cid][tid]
+        if FRONTEND_svc not in single_trace or DETAIL_svc not in single_trace:
+            if FRONTEND_svc not in single_trace:
+                print("no frontend")
+            if DETAIL_svc not in single_trace:
+                print("no detail")
+            removed_traces_.append(single_trace)
+            for svc, sp in single_trace.items():
+                print(svc, " ")
+                sp.print()
+            print()
+            what[0] += 1
+        elif len(single_trace) < MIN_TRACE_LEN:
+            removed_traces_.append(single_trace)
+            what[1] += 1
+        elif len(single_trace) > MAX_TRACE_LEN:
+            removed_traces_.append(single_trace)
+            what[2] += 1
+        elif len(single_trace) == MIN_TRACE_LEN and (REVIEW_V1_svc not in single_trace or REVIEW_V2_svc in single_trace or REVIEW_V3_svc in single_trace):
+            removed_traces_.append(single_trace)
+            what[3] += 1
+        elif len(single_trace) == MAX_TRACE_LEN and REVIEW_V2_svc not in single_trace and REVIEW_V3_svc not in single_trace:
+            removed_traces_.append(single_trace)
+            what[4] += 1
+        elif single_trace[FRONTEND_svc].parent_span_id != span_id_of_FRONTEND_svc:
+            removed_traces_.append(single_trace)
+            what[5] += 1
+        elif FILTER_REVIEW_V1 and REVIEW_V1_svc in single_trace:
+            if len(single_trace) != 3:
+                print_single_trace(single_trace)
+            assert len(single_trace) == 3
+            removed_traces_.append(single_trace)
+            what[6] += 1
+        elif FILTER_REVIEW_V2 and REVIEW_V2_svc in single_trace:
+            if len(single_trace) != 4:
+                print_single_trace(single_trace)
+            assert len(single_trace) == 4
+            removed_traces_.append(single_trace)
+            what[7] += 1
+        elif FILTER_REVIEW_V3 and REVIEW_V3_svc in single_trace:
+            if len(single_trace) != 4:
+                print_single_trace(single_trace)
+            assert len(single_trace) == 4
+            removed_traces_.append(single_trace)
+            what[8] += 1
+        else:
+            ret_traces_.append(single_trace)
+    print("filter stats: ", what)
+    assert input_trace_len == len(ret_traces_) + len(removed_traces_)
+    print("#input trace: " + str(input_trace_len))
+    print("#filtered trace: " + str(len(ret_traces_)))
+    print("#removed_traces trace: " + str(len(removed_traces_)))
+    return ret_traces_, removed_traces_
+
+def change_to_relative_time(traces_):
+    for cid, tid in traces_.items():
+        for svc, span in traces_[cid][tid].items():
+            if svc == FRONTEND_svc:
+                base_t = span.start
+        for svc, span in traces_[cid][tid].items():
+            span.start -= base_t
+            span.end -= base_t
+    return traces_
+
+
+def print_single_trace(tra):
+    for svc, span in tra.items():
+        span.print()
+
+def print_dag(single_dag_):
+    for parent_span, children in single_dag_.items():
+        for child_span in children:
+            print("{}({})->{}({})".format(parent_span.svc_name, parent_span.my_span_id, child_span.svc_name, child_span.my_span_id))
+            
+'''
+parallel-1
+    ----------------------A
+        -----------B
+           -----C
+parallel-2
+    ----------------------A
+        --------B
+             ---------C
+'''
+def is_parallel_execution(span_a, span_b):
+    assert span_a.parent_span_id == span_b.parent_span_id
+    if span_a.st < span_b.st:
+        earlier_start_span = span_a
+        later_start_span = span_b
+    else:
+        earlier_start_span = span_b
+        later_start_span = span_a
+    if earlier_start_span.et > later_start_span.st and later_start_span.et > earlier_start_span.st: # parallel execution
+        if earlier_start_span.st < later_start_span.st and earlier_start_span.et > later_start_span.et: # parallel-1
+            return 1
+        else: # parallel-2
+            return 2
+    else: # sequential execution
+        return 0
+    
+
+def single_trace_to_callgraph(single_trace_):
+    cg = dict()
+    svc_list = list()
+    for svc, parent_span in single_trace_.items():
+        svc_list.append(svc)
+        cg[parent_span] = list()
+        for svc, span in single_trace_.items():
+            if span.parent_span_id == parent_span.my_span_id:
+                cg[parent_span].append(span)
+        if len(cg[parent_span]) == 0:
+            del cg[parent_span]
+    svc_list.sort()
+    cg_key = ""
+    for svc in svc_list:
+        cg_key += svc+","
+    return cg, cg_key
+
+                
+def calc_exclusive_time(single_trace_):
+    for svc, parent_span in single_trace_.items():
+        child_span_list = list()
+        for svc, span in single_trace_.items():
+            if span.parent_span_id == parent_span.my_span_id:
+                child_span_list.append(span)
+        if len(child_span_list) == 0:
+            continue
+        exclude_child_rt = 0
+        if  len(child_span_list) == 1:
+            print("parent: {}, child_1: {}, child_2: None, single child".format(parent_span.svc_name, child_span_list[0].svc_name))
+            exclude_child_rt = child_span_list[0].rt
+        else: # else is redundant but still I leave it there to make the if/else logic easy to follow
+            for i in range(len(child_span_list)):
+                for j in range(i+1, len(child_span_list)):
+                    is_parallel = is_parallel_execution(child_span_list[i], child_span_list[j])
+                    if is_parallel == 1 or is_parallel == 2: # parallel execution
+                        # TODO: parallel-1 and parallel-2 should be dealt with individually.
+                        exclude_child_rt = max(child_span_list[i].rt, child_span_list[j].rt)
+                        print("parent: {}, child_1: {}, child_2: {}, parallel-{} sibling".format(parent_span.svc_name, child_span_list[i].svc_name, child_span_list[j].svc_name, is_parallel))
+                    else: # sequential execution
+                        exclude_child_rt = child_span_list[i].rt + child_span_list[j].rt
+                        print("parent: {}, child_1:{}, child_2: {}, sequential sibling".format(parent_span.svc_name, child_span_list[i].svc_name, child_span_list[j].svc_name))
+        parent_span.xt = parent_span.rt - exclude_child_rt
+        if parent_span.xt < 0:
+            print("parent_span")
+            parent_span.print()
+            print("child_span_list")
+            for span in child_span_list:
+                span.print()
+            print_error("parent_span exclusive time cannot be negative value: {}".format(parent_span.xt))
+    return single_trace_
+
+
+def traces_to_graphs_and_calc_exclusive_time(traces_):
+    graph_dict = dict()
+    for cid, tid in traces_.items():
+        single_trace = traces_[cid][tid]
+        callgraph, cg_key = single_trace_to_callgraph(single_trace)
+        single_trace_ex_time = calc_exclusive_time(single_trace)
+        graph_dict[cg_key] = callgraph
+    assert len(graph_dict) == 1
+    return callgraph
+
+
+# def add_child_services(graph_dict_):
+#     for tid in graph_dict_:
+#         dag = graph_dict_[tid]
+#         for parent_span, children in dag.items():
+#             for child_span in children:
+#                 parent_span.child_spans.append(child_span)
+
+# def get_unique_dag_list(graph_dict_):
+#     unique_dags = dict()
+#     for _, dag in graph_dict_.items():
+#         temp_list = list()
+#         for parent_span, children in dag.items():
+#             for child_span in children:
+#                 temp_list.append((parent_span.svc_name + "," + child_span.svc_name))
+#         temp_list.sort()
+#         temp_str = ""
+#         for elem in temp_list:
+#             temp_str += elem + ","
+#         if temp_str not in unique_dags:
+#             unique_dags[temp_str] = dag
+#     print(" --- unique dag list --- ")
+#     i = 0
+#     for _, dag in unique_dags.items():
+#         print("unique dag #"+str(i))
+#         print_dag(dag)
+#         i += 1
+#     return unique_dags
+
+def get_unique_svc_names_from_dag(dag_):
+    unique_svc_names = dict()
+    for parent_span, children in dag_.items():
+        for child_span in children:
+            unique_svc_names[parent_span.svc_name] = "xxxx"
+            unique_svc_names[child_span.svc_name] = "xxxx"
+    return unique_svc_names
+
+def stitch_time(traces):
+    print_log("time stitching starts")
+    ts = time.time()
+    traces, removed_traces = remove_incomplete_trace(traces)
+    traces = change_to_relative_time(traces)
+    call_graph = traces_to_graphs_and_calc_exclusive_time(traces)
+    # add_child_services(graph_dict)
+    
+    print("*"*50)
+    for cid, tid in traces.items():
+        print("="*30)
+        print("Trace: " + tid)
+        for svc, span in traces[cid][tid]:
+                span.print()
+        print("="*30)
+    print()
+    print("num final valid traces: " + str(len(traces)))
+    
+    print_log("time stitching done: {}s".format(time.time() - ts))
+    return traces, call_graph
+
+if __name__ == "__main__":
+    traces, call_graph = stitch_time(LOG_PATH)
